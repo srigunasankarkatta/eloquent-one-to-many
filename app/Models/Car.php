ef6fae8eda27241140d3b0c85c72452137ead369
srigunasankarkatta <kattasrigunasankar@gmail.com>
2022-09-14 23:42:14 +0530

    project set-up

---
+++ app/Models/Car.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Car extends Model
+{
+    use HasFactory;
+    protected $table = 'cars';
+    protected $primaryKey = 'id';
+    protected $fillable = ['name', 'founded', 'description'];
+
+    public function carModels(){
+        return $this->hasMany(CarModel::class);
+    }
+
+}
